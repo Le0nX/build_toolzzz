1d44b3f38bf78fce43e451d9ada3ef80238511ce
Le0nX <cloudmindone@gmail.com>
2018-04-10 19:34:38 +0300

    fix: bash vs shell

--- version.mk
+++ version.mk
@@ -4,6 +4,7 @@ BRANCH := $(shell git branch | grep \* | cut -d ' ' -f2)
 CHANGES := $(shell git status --porcelain)
 
 version:
+	SHELL:=/bin/bash
 	@echo Building version
 	@echo Branch: $(BRANCH)
 	@echo Commit: $(COMMIT)
