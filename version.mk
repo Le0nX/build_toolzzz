42aa9ab4f4c022ece3b7792126126da6514cf0d2
Le0nX <cloudmindone@gmail.com>
2018-04-10 19:43:01 +0300

    fix: again...

--- version.mk
+++ version.mk
@@ -1,10 +1,10 @@
 #Дополнительная информация о ветке и ее текущем состоянии.
+SHELL=/bin/bash
 COMMIT := $(shell git rev-parse --verify --short HEAD)
 BRANCH := $(shell git branch | grep \* | cut -d ' ' -f2)
 CHANGES := $(shell git status --porcelain)
 
 version:
-	SHELL:=/bin/bash
 	@echo Building version
 	@echo Branch: $(BRANCH)
 	@echo Commit: $(COMMIT)
